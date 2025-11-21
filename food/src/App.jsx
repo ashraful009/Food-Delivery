905b6f71b3e7e2580aead6fc78c140f7e19fe7e9
Md_AsHrAFuL_IsLsM <121592381+ashraful009@users.noreply.github.com>
2025-11-22 01:16:21 +0600

    initialize project

---
+++ food/src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from "./components/Navbar/Navbar.jsx"
+import ProductInfo from "./components/FoodDescription/ProductInfo.jsx"
+import ProductPage from "./pages/ProductPage.jsx"
+import ProductCard from "./components/HomePage/ProductCard/ProductCard.jsx"
+import LeftBar from "./components/HomePage/LeftBar/LeftBar.jsx"
+import Order from "./components/Order/Order.jsx"
+function App() {
+
+  return (
+    <>
+      <Order />
+      <Order />
+      <Order />
+      <Order />
+      <Order/>
+    </>
+  )
+}
+
+export default App
